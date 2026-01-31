3ca12d262ec2e22f32180a2633e2d4f11964d54a
ashna-benny <ashnabenny24@gmail.com>
2026-01-31 13:42:12 +0530

    Update project01.c

--- project01.c
+++ project01.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
-int a,b,sum;
+int a,b,c,sum;
 printf("enter the first number:");
 scanf("%d",&a);
+  printf("enter the third number:");
+scanf("%d",&c);
 printf("enter the second number:");
 scanf("%d",&b);
-sum=a+b;
+sum=a+b+c;
 printf("sum=%d",sum);
 return 0;
 }
